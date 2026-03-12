5fc873e3aef13106bb359cfc653b61bdf26f003a
Ana Flechas <aflechast@unbosque.edu.co>
2026-03-11 23:01:34 -0500

    Conexión auth y usuarios

--- ms-usuarios/src/main/java/com/tiendagenerica/ms_usuarios/security/SecurityConfig.java
+++ ms-usuarios/src/main/java/com/tiendagenerica/ms_usuarios/security/SecurityConfig.java
@@ -11,21 +11,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)
-            throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        // Este endpoint lo consulta MS-Auth internamente
-                        .requestMatchers("/usuarios/credenciales/**")
-                        .permitAll()
-                        // Todos los demás requieren autenticación
-                        .anyRequest().permitAll()
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.csrf(csrf -> csrf.disable())
+
+				.authorizeHttpRequests(auth -> auth
+
+						// 🔓 Swagger
+						.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
+								"/swagger-resources/**", "/webjars/**")
+						.permitAll()
+
+						// 🔓 Endpoint usado por ms-auth
+						.requestMatchers("/usuarios/credenciales/**").permitAll()
+
+						// (Mientras pruebas puedes dejar todo abierto)
+						.anyRequest().permitAll())
+
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		return http.build();
+	}
 }
